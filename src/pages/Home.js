f24f8fc5c10d343809b7c7f518354751bb65d62e
ms9648 <ms9648@naver.com>
2024-05-07 01:49:30 +0900

    [feat] 바코드 입력 공간 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,22 @@ import '../assets/css/App.css';
 
 export default function Home() {
     return (
-        <>
+        <div id='home_body'>
             <Navbar />
-        </>
-        
+            <div className='container-home'>
+                <div className='container-product-list'></div>
+                <div className='container-calculate'>
+                    {/* 바코드 찍은 목록 */}
+                    <div className='calculate-header'>선택한 품목</div>
+                    <div className='wrapper-calculate-list'>
+
+                    </div>
+                    <div className='wrapper-barcode'>
+                        <div className='header-barcode'>바코드 입력</div>
+                        <input className='input-barcode' placeholder='🛒 바코드를 입력해주세요.'></input>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
